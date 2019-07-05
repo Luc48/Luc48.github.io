a63dfa846c4f39a455d9ee11a4a9e92e00101350
Luc48 <52536680+Luc48@users.noreply.github.com>
2019-07-05 14:39:24 +0200

    Add files via upload

---
+++ data/Parkings_1.js
@@ -0,0 +1,25 @@
+var json_Parkings_1 = {
+"type": "FeatureCollection",
+"name": "Parkings_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "qc_id": 0.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.730814934222186, 44.362437502804625 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 1.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.726313371880788, 44.362216059213218 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 2.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.725936520904958, 44.362011223145913 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 3.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.699603413676169, 44.366794235224759 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 4.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.692884625387937, 44.363631438412732 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 5.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.684338367984044, 44.363592686634398 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 6.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.685610885320232, 44.361693818095368 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 7.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.62985759026614, 44.352569533163262 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 8.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.629501388658851, 44.349383819169688 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 9.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.511593494303715, 44.351827568895317 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 10.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.512465930124469, 44.345688469342761 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 11.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.437286741617736, 44.357834994752885 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 12.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.712150486234411, 44.503292002294494 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 13.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.714076039850632, 44.504160908865892 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 14.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.702858270392063, 44.500928216340441 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 15.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.697298427913057, 44.501403190371008 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 16.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.609481825860727, 44.520049305878402 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 17.0, "id": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.663640043287981, 44.342338888548262 ] ] } }
+]
+}
